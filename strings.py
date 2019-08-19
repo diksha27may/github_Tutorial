f03f1093390721a2d51cdb00edcaef25dd53a6f4
diksha27may <40164404+diksha27may@users.noreply.github.com>
2019-08-19 15:46:59 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,48 @@
+var1 = "Guru99!"
+var2 = "Software Testing"
+print ("var1[0]:",var1[0])
+print ("var2[1:5]:",var2[1:5])
+
+
+
+x = "Hello World!"
+print(x[:6]) 
+print(x[0:6] + "Guru99")
+
+
+
+oldstring = 'I like Guru99' 
+newstring = oldstring.replace('like', 'love')
+print(newstring)
+
+
+
+
+string="python at guru99"
+print(string.upper())
+
+
+
+
+string="python at guru99"		
+print(string.capitalize())
+
+
+
+string="PYTHON AT GURU99"
+print(string.lower())
+
+
+print(":".join("Python"))
+
+
+word="guru99 career guru99"		
+print(word.split(' '))
+
+string="12345"		
+print(''.join(reversed(string)))
+
+
+x = "Guru99"
+x = x.replace("Guru99","Python")
+print(x)
