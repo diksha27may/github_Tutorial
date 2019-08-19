d8dd997a3a9a607d1af1e398f07b28914f943814
diksha27may <40164404+diksha27may@users.noreply.github.com>
2019-08-19 16:03:10 +0530

    Add files via upload

---
+++ python operators.py
@@ -0,0 +1,27 @@
+x= 4	
+y= 5
+print(x + y)
+
+
+x = 4
+y = 5
+print(('x > y  is',x>y))
+
+
+x = 20
+y = 20
+if ( x is y ): 
+	print("x & y  SAME identity")
+y=30
+if ( x is not y ):
+	print("x & y have DIFFERENT identity")
+
+
+
+v = 4
+w = 5
+x = 8
+y = 2
+z = 0
+z = (v+w) * x / y;   
+print("Value of (v+w) * x/ y is ",  z)
