bf55e1101d6a28e9d33bb2de266abd6a8f408b60
diksha27may <40164404+diksha27may@users.noreply.github.com>
2019-08-19 15:37:56 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,34 @@
+a=100 
+print (a)
+
+
+# Declare a variable and initialize it
+f = 0
+print(f)
+# re-declaring the variable works
+f = 'guru99'
+print(f)
+
+
+
+a="Guru"
+b = 99
+print(a+str(b))
+
+
+# Declare a variable and initialize it
+f = 101
+print(f)
+# Global vs. local variables in functions
+def someFunction():
+# global f
+    f = 'I am learning Python'
+    print(f)
+someFunction()
+print(f)
+
+
+f = 11;
+print(f)
+del f
+print(f)
