4cfcfea9eb5b8f47af104eba2a6a8343cbdf9d5b
diksha27may <40164404+diksha27may@users.noreply.github.com>
2019-08-19 15:51:48 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,26 @@
+tup1 = ();
+
+Tup1 = (50,);
+
+
+tup1 = ('Robert', 'Carlos','1965','Terminator 1995', 'Actor','Florida');
+tup2 = (1,2,3,4,5,6,7);
+print(tup1[0])
+print(tup2[1:4])
+
+
+x = ("Guru99", 20, "Education")    # tuple packing
+(company, emp, profile) = x    # tuple unpacking
+print(company)
+print(emp)
+print(profile)
+
+
+a=(5,6)
+b=(1,4)
+if (a>b):print("a is bigger")
+else: print("b is bigger")
+
+
+x = ("a", "b","c", "d", "e")
+print(x[2:4])
